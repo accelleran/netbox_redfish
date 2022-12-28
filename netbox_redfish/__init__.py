fbc7435e1295b6b29ca230be1bd6c7825b6e9553
Ilias Barthelemy <ilias.barthelemy@accelleran.com>
2022-12-28 15:06:26 +0000

    Changed the __init__.py file

--- netbox_redfish/__init__.py
+++ netbox_redfish/__init__.py
@@ -5,6 +5,6 @@ class NetboxRedfishConfig(PluginConfig):
     verbose_name = 'Use netbox in combination with Redfish OOB interfaces'
     description = 'Use netbox in combination with Redfish OOB interfaces'
     version = '0.1'
-    base_url = 'redfish'
+    base_url = 'netbox_redfish'
 
 config = NetboxRedfishConfig
